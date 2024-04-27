666899850e2cff537632cae11662905707295aa3
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-27 18:47:01 +0530

    m

--- backend.sh
+++ backend.sh
@@ -8,44 +8,39 @@ echo "Please enter DB password:"
 read -s mysql_root_password
  
 
-dnf module disable nodejs -y &>>LOGFILE
-
-dnf module enable nodejs:20 -y &>>LOGFILE
-
+dnf module disable nodejs -y &>>$LOGFILE
+dnf module enable nodejs:20 -y &>>$LOGFILE
 dnf install nodejs -y &>>$LOGFILE
 
-id expense
+id expense &>>$LOGFILE
 if [ $? -ne 0 ]
-then 
-   useradd expense
-   VALIDATE $? "Creating expense user"
+then
+    useradd expense &>>$LOGFILE
 else
-   echo -e "Expense user already created....$Y SKIPPING $N"
-fi 
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOGFILE
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "Dowloading Backend code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
 
-npm install &>>LOGFILE
+npm install &>>$LOGFILE
 
 #check your repo and path
-cp /home/ec2-user/Shell-Project/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/Expense-Shell1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
+systemctl daemon-reload &>>$LOGFILE
 
-systemctl daemon-reload &>>LOGFILE
+systemctl start backend &>>$LOGFILE
 
-systemctl start backend &>>LOGFILE
+systemctl enable backend &>>$LOGFILE
 
-systemctl enable  backend &>>LOGFILE
+dnf install mysql -y &>>$LOGFILE
 
-dnf install mysql -y &>LOGFILE
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 
-mysql -h db.devops-practice.cloud -u root -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-
-systemctl restart backend &>LOGFILE
+systemctl restart backend &>>$LOGFILE
