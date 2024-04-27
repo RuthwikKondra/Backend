ece3763e603aaf9d1c7301d51e17d1989bf20dd5
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-27 13:01:20 +0530

    a

--- mysql.sh
+++ mysql.sh
@@ -7,18 +7,14 @@ check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-
 dnf install mysql-server -y &>>$LOGFILE
 
 systemctl enable mysqld &>>$LOGFILE
 
 systemctl start mysqld &>>$LOGFILE
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
-
 #Below code will be useful for idempotent nature
-mysql -h  172.31.17.245 -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.17.245 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
