65bb63d24db2032b27849329aab5084090564f28
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-27 13:09:44 +0530

    d

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,7 @@ systemctl enable mysqld &>>$LOGFILE
 systemctl start mysqld &>>$LOGFILE
 
 #Below code will be useful for idempotent nature
-mysql -h 172.31.17.245 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.devops-practice.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
